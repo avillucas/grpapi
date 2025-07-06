917fdc666a9b3ad0de961afc48cafdfda479499e
Lucas <lucas@dex360.net>
2025-07-06 10:58:31 -0300

    api test

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request){
+        $user = User::where('email', $request->input('email'))->first();
+        if ($user && Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Login successful', 'user' => $user,'token'  => $user->createToken('api')->plainTextToken], 200);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+}});
